191396d4aea71fe1c163cfc225937fcd83e1024e
Lucdijord <lucdj678@gmail.com>
2025-11-09 02:24:39 -0300

    relatorio e cadastro pipas feito

---
+++ js/relatorios.js
@@ -0,0 +1,23 @@
+
+// RELATÓRIO DE PIPAS CADASTRADAS
+function carregarRelatorioPipas() {
+    const tabela = document.getElementById("tabelaPipas");
+    tabela.innerHTML = "";
+
+    const pipas = JSON.parse(localStorage.getItem("pipas")) || [];
+
+    pipas.forEach(pipa => {
+        const linha = `
+            <tr>
+                <td>${pipa.id}</td>
+                <td><img src="${pipa.imagem}" alt="Pipa ${pipa.id}"></td>
+                <td>R$ ${pipa.preco}</td>
+            </tr>
+        `;
+
+        tabela.innerHTML += linha;
+    });
+}
+
+carregarRelatorioPipas();
+// FIM RELATÓRIO DE PIPAS CADASTRADAS
